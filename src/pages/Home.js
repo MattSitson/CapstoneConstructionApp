549ab1ac342af7bfa1e105ab9479280f8da1dc90
MattSitson <97920773+MattSitson@users.noreply.github.com>
2024-01-05 19:56:52 -0500

    update test 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,8 +12,8 @@ const Home = () => {
 
   useEffect(() => {
     Axios.get('https://mysql-capstone-509eaa3bcdeb.herokuapp.com/login').then((response)=> {
-      console.log(response); if(response.data.loggedIn === false){
-          //navigate('/');
+      console.log(response); if(response.status != 200){
+          navigate('/');
           
       }
       });
